5f5a9390f8541eef946432158b9c4a37360a2e02
Gabriel Schultz <gabrielschultz2807@gmail.com>
2025-12-30 16:24:05 -0300

    feat: ciclo de vida e rfv

--- src/analytics/frequencia_valor.sql
+++ src/analytics/frequencia_valor.sql
@@ -1,12 +1,33 @@
-SELECT  IdCliente,
-        count(DISTINCT substr(DtCriacao, 0, 11)) as qtdeFrequencia,
-        sum(CASE WHEN QtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPos,
-        sum(abs(QtdePontos)) as QtdePontosAbs
+WITH tb_freq_valor AS (
 
-FROM transacoes
+    SELECT  IdCliente,
+            count(DISTINCT substr(DtCriacao, 0, 11)) as qtdeFrequencia,
+            sum(CASE WHEN QtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPos
+            -- sum(abs(QtdePontos)) as QtdePontosAbs
 
-WHERE date(DtCriacao) < '2025-09-01'
-AND DtCriacao >= date('2025-09-01',  '-28 days') 
+    FROM transacoes
 
-GROUP BY IdCliente
-ORDER BY qtdeFrequencia desc
+    WHERE date(DtCriacao) < '2025-09-01'
+    AND DtCriacao >= date('2025-09-01',  '-28 days') 
+
+    GROUP BY IdCliente
+    ORDER BY qtdeFrequencia DESC
+),
+
+tb_cluster AS(
+    SELECT  *,
+            CASE
+                WHEN qtdeFrequencia <= 10 AND qtdePontosPos >= 1500 THEN 'HYPER'
+                WHEN qtdeFrequencia > 10 AND qtdePontosPos >= 1500 THEN 'EFICIENTE'
+                WHEN qtdeFrequencia <= 10 AND qtdePontosPos >= 750 THEN 'INDECISO'
+                WHEN qtdeFrequencia > 10 AND qtdePontosPos >= 750 THEN 'ESFORÇADO'
+                WHEN qtdeFrequencia < 5 THEN 'LURKER'
+                WHEN qtdeFrequencia <= 10 THEN 'PREGUIÇOSO'
+                WHEN qtdeFrequencia > 10 THEN 'POTENCIAL' 
+
+            END AS cluster
+
+    FROM tb_freq_valor
+)
+
+SELECT * FROM tb_cluster
